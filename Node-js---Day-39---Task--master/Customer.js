73f2e3e99406f60e8d3ca8f33f7244ef91dd3f72
elasiddhu <101243835+elasiddhu@users.noreply.github.com>
2022-10-08 07:48:59 +0530

    Add files via upload

---
+++ Node-js---Day-39---Task--master/Customer.js
@@ -0,0 +1,16 @@
+var Customers = [
+    { 
+      id: 1,
+      name: "Arun", 
+      address: "Delhi", 
+      isActive: true 
+    },
+    { 
+      id: 2, 
+      name: "Vivek", 
+      address: "Mumbai", 
+      isActive: false 
+    },
+  ];
+  
+  module.exports = Customers;
---
+++ Node-js---Day-39---Task--master/Room.js
@@ -0,0 +1,18 @@
+let Rooms = [
+    {
+      id: 1,
+      name: "Main Hall - 1",
+      location: "Floor 10",
+      facilities: ["A/c", "Parking", "Catering Service"],
+      oneHourPrice: 5000,
+    },
+    {
+      id: 2,
+      name: "Main Hall - 2",
+      location: "Floor 6",
+      facilities: ["Parking", "A/c", "24/7 Support"],
+      oneHourPrice: 3000,
+    },
+  ];
+  
+  module.exports = Rooms;
